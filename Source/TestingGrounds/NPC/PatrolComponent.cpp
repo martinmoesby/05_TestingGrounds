937bd50a95504fdf0a8c3c94509e852c11a46b7e
WORKSTATION\Martin <martinmoesby@martinmoesby.com>
2018-04-14 12:16:58 +0200

    Adding gun to NPC

---
+++ Source/TestingGrounds/NPC/PatrolComponent.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PatrolComponent.h"
+
+
+
+TArray<AActor*> UPatrolComponent::GetPatrolPoints() const
+{
+	return PatrolPoints;
+}
